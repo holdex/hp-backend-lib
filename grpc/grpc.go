9a118aa4b7bd1b4e8b5efa646bcfc28525527030
Mihail Ozarinschi <mihail.ozarinschi@outlook.com>
2019-03-26 14:32:14 +0200

    hotfix/HP-1122: allow grpc client to set dial options

--- grpc/grpc.go
+++ grpc/grpc.go
@@ -10,16 +10,16 @@ import (
 	"google.golang.org/grpc/metadata"
 )
 
-func Dial(addr string) *grpc.ClientConn {
-	conn, err := grpc.Dial(addr, grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
-			if imd, ok := metadata.FromIncomingContext(ctx); ok {
-				for k, v := range imd {
-					ctx = metadata.AppendToOutgoingContext(ctx, k, v[0])
-				}
+func Dial(addr string, opts ...grpc.DialOption) *grpc.ClientConn {
+	opts = append(opts, grpc.WithUnaryInterceptor(func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		if imd, ok := metadata.FromIncomingContext(ctx); ok {
+			for k, v := range imd {
+				ctx = metadata.AppendToOutgoingContext(ctx, k, v[0])
 			}
-			return invoker(ctx, method, req, reply, cc, opts...)
-		}))
+		}
+		return invoker(ctx, method, req, reply, cc, opts...)
+	}))
+	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		liblog.Fatalf("failed to dial %s: %v", addr, err)
 	}
