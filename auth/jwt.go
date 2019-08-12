0d59a8ad5b3eb3f40665a0c0ccf76dc322b1deb7
mihailozarinschi <mihail.ozarinschi@outlook.com>
2019-08-12 15:12:59 +0200

    fix: remove authv2 param from context

--- auth/jwt.go
+++ auth/jwt.go
@@ -47,48 +47,6 @@ func MakeJWTValidator(jwkSet oidc.KeySet, issuer string, audiences ...string) JW
 	}
 }
 
-func MakeJWTValidator2(jwkSet1, jwkSet2 oidc.KeySet, issuer1, issuer2 string, audiences ...string) JWTValidator {
-	return func(ctx context.Context) (context.Context, error) {
-		token, err := extractAuth(ctx, "bearer")
-		if err != nil {
-			return ctx, err
-		}
-
-		var jwkSet oidc.KeySet
-		if libctx.IsAuthV2(ctx) {
-			jwkSet = jwkSet2
-		} else {
-			jwkSet = jwkSet1
-		}
-		payload, err := jwkSet.VerifySignature(ctx, token)
-		if err != nil {
-			return ctx, status.Error(codes.Unauthenticated, err.Error())
-		}
-
-		claims := jwt.Claims{}
-		if err := json.Unmarshal(payload, &claims); err != nil {
-			return ctx, status.Error(codes.Unauthenticated, err.Error())
-		}
-
-		// Validate iss, exp, nbf
-		var issuer string
-		if libctx.IsAuthV2(ctx) {
-			issuer = issuer2
-		} else {
-			issuer = issuer1
-		}
-		if err := claims.ValidateWithLeeway(jwt.Expected{
-			Issuer:   issuer,
-			Time:     time.Now(),
-			Audience: audiences,
-		}, 0); err != nil {
-			return ctx, status.Errorf(codes.Unauthenticated, "validation failed: %s", err.Error())
-		}
-
-		return libctx.WithAuthSubject(ctx, claims.Subject), nil
-	}
-}
-
 func extractAuth(ctx context.Context, expectedScheme string) (string, error) {
 	val := libctx.GetVal(ctx, "authorization")
 	if val == "" {
